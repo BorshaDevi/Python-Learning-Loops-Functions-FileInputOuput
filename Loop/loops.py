f64c9810e7126090fb76255aea07e63dfa905b52
Borsha Devi <borshadevi27@gmail.com>
2025-12-29 11:06:40 +0600

    Now I learned about a little bit while loop which is object that we can loop through

---
+++ Loop/loops.py
@@ -0,0 +1,10 @@
+# Loops are used to repeat instructions.
+# While and For  .There are two types of loops.
+
+
+# While 
+# A while loop repeatedly executes a block of code as long as the condition is true.
+# Once the condition becomes False, the loop stops.
+while condition:
+           print("hello")
+
