76a9165be56b53fd5bd5cd5f70bfdc4b2bbd4967
Borsha Devi <borshadevi27@gmail.com>
2025-12-30 09:49:55 +0600

    Now I learned about while loop

--- Loop/loops.py
+++ Loop/loops.py
@@ -5,6 +5,20 @@
 # While 
 # A while loop repeatedly executes a block of code as long as the condition is true.
 # Once the condition becomes False, the loop stops.
-while condition:
-           print("hello")
+count=1
+while count <=5:
+           print('hello')
+           count +=1
+# hello
+# hello
+# hello
+# hello
+# hello
+
+# Here count variable is a iterator.
+# And Running in the loop that is called iteration.  1 iteration means 1 loop completed.
+i=10
+while i>=1:
+        print(i)
+        i -=1
 
