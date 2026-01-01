f483588cec2c420b123c3bcfa8d6768c243e8f38
Borsha Devi <borshadevi27@gmail.com>
2026-01-01 11:06:43 +0600

    now i learned about for loop else which is use when we need to do something after the loop complete this time we use else

--- Loop/for loop.py
+++ Loop/for loop.py
@@ -16,3 +16,36 @@ for str in string:
 tu=(1,23,4,6,5,7,8,9) 
 for t in tu:
     print(t)   
+
+# we can use else with for loop in the end.It's completely optional.If you do something When loop complete then use else.In the else you write anything you need.
+
+value=[1,2,3,4,5,6]
+for va in value:
+    print(va)
+else:
+    print('end')
+
+# well,when use break these time need else
+
+# search e letter;
+string='Bangladesh'
+for str in string:
+    if(str == "e"):
+        print('found')
+        break
+    print(str)
+else:
+    print('End')    
+
+# when e is found the loop stop and nothing print.
+
+string='Bangladesh'
+for str in string:
+    if(str == "e"):
+        print('found')
+        break
+    print(str)
+
+print('End')    
+# see here when we  found e then "End " also print.That why use else.
+# If we want to do something after the loop finishes, we write it inside the else block.
