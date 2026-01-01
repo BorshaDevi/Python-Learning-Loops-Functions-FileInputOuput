ec822cd4f3b5a786a8d8c7deff21a4e3b4bf17cb
Borsha Devi <borshadevi27@gmail.com>
2026-01-01 10:38:55 +0600

    Now I learned about for loop which is sequential traversal

---
+++ Loop/for loop.py
@@ -0,0 +1,18 @@
+#  For loop are used for sequential traversal.For traversing list,string,tuples etc.
+# Sequential traversal means 1st then 2nd 3rd like this.
+# It seems like an index number. Index numbers continue 1st 0 then 1 like this.
+nums=[1,4,5,3,26,7,3,8]
+for val in nums:
+    print(val)
+
+vegetables=['potato','cucumber','latis','ladyfingers']
+for val in vegetables:
+    print(val)
+
+string='Chattogram' 
+for str in string:
+    print(str)   
+
+tu=(1,23,4,6,5,7,8,9) 
+for t in tu:
+    print(t)   
