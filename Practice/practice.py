420aeff8f9717cb95504281c87a5b9a77b5f5332
Borsha Devi <borshadevi27@gmail.com>
2026-01-03 11:21:03 +0600

    Now I practice range funcation in the practice.py file.(8 to 10)

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,40 +1,40 @@
-# # 1. print numbers from 1 to 100
-# x=1
-# while x <=100:
-#     print(x)
-#     x +=1
+# 1. print numbers from 1 to 100
+x=1
+while x <=100:
+    print(x)
+    x +=1
     
-# # 2.Print numbers from 100 to 1
-# y=100
-# while y>=1:
-#     print(y)
-#     y -=1
+# 2.Print numbers from 100 to 1
+y=100
+while y>=1:
+    print(y)
+    y -=1
 
-# # 3.print the multiplication table of  a number n.
-# n=int(input())
-# i=1
-# while i<=10:
-#     new=n*i
-#     print(new)
-#     i +=1
+# 3.print the multiplication table of  a number n.
+n=int(input())
+i=1
+while i<=10:
+    new=n*i
+    print(new)
+    i +=1
 
-# # 4.Print the elements of the following list using a loop:[1,4,9,16,25,36,49,64,81,100]
-# list1=[1,4,9,16,25,36,49,64,81,100]
-# i=0
-# while i<=len(list1)-1:
-#     print(list1[i])
-#     i+=1
+# 4.Print the elements of the following list using a loop:[1,4,9,16,25,36,49,64,81,100]
+list1=[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i<=len(list1)-1:
+    print(list1[i])
+    i+=1
 
-# # 5.Search for a number x in this tuple using loop:(1,4,9,16,25,36,49,64,81,100)
-# list2=(1,4,9,16,25,36,49,64,81,100)
-# x=int(input())
-# i=0
-# while i<=len(list2)-2:
-#     if(x==(list2[i])):
-#         print("found",i)
-#     else:
-#         print('not found')    
-#     i +=1
+# 5.Search for a number x in this tuple using loop:(1,4,9,16,25,36,49,64,81,100)
+list2=(1,4,9,16,25,36,49,64,81,100)
+x=int(input())
+i=0
+while i<=len(list2)-2:
+    if(x==(list2[i])):
+        print("found",i)
+    else:
+        print('not found')    
+    i +=1
 
 
 
@@ -57,3 +57,21 @@ for li in list2:
         print("found",x)
         break
     print(li)    
+
+
+# range()
+
+# 8. print numbers from 1 to 100.
+for i in range(1,101):
+    print(i)
+        
+# 9. print numbers from 100 to 1.
+for i in range(100):
+    print(100-i)
+for i in range(100,0,-1):
+    print(i)
+
+# 10. print the multiplication table of a number n.
+n=int(input("n:"))
+for i in range(1,11):
+    print(n*i)
