97d3fe354c678b07e9ea35aea2b74d02bee4b9a6
Borsha Devi <borshadevi27@gmail.com>
2026-01-05 10:30:59 +0600

    now I learned about build-in function which is provided python by default and create default parameters.py file

---
+++ Functions/functions types.py
@@ -0,0 +1,31 @@
+# In python , there are 2 types of function.
+# 1.Built-in function 
+# 2.user defined function.
+
+
+
+
+# Built-in function: 
+# This function already  provided the python and can be used directly without defining them.
+# Ex: print() , len() , type(), input() , range()
+
+# print()
+
+# if we hover print() function,we can about the function.
+
+print('Hello','Joya', sep=' $ ')
+# Hello $ Joya
+
+print('Hello','world') #sep=''  # here sep parameter automatically separated them this 2 value.
+# Hello world
+print('hello' , end=' ') #end='\n'  # if we need to print this 2 print value ,then write end=' '
+# parameter in the first print function pass the value space.
+# hello Good Morning   
+print('Good Morning')
+
+print('Potato' , end=' $ ') # we can pass anything in the end parameter.
+print('Tomato' , end=' $ ')
+print('Cucumber')
+# Potato $ Tomato $ Cucumber
+
+
