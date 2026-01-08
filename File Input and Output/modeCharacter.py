03309d9c55aaebe43a8f28d0eb8cae04fe5b6541
Borsha Devi <borshadevi27@gmail.com>
2026-01-08 10:26:50 +0600

    create modeCharacter.py file for understand mode case

---
+++ File Input and Output/modeCharacter.py
@@ -0,0 +1,7 @@
+# 1.   'r' - open reading (default) 
+# 2.    'w' - open for writing , truncating the file fist
+# 3.    'x' -  create a new file and open it for writing .
+# 4.     'a' -  open for writing , appending to the end of the file if it exists.
+# 5.     'b' -   binary mode
+# 6.      't' - text mode (by default)
+# 7.      "+" - open disk file for updating (reading and writing)
