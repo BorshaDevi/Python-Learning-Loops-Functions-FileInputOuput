37f79d9845aa71dc6a9357d57dd1930e6b0a5b0e
Borsha Devi <borshadevi27@gmail.com>
2026-01-08 10:13:55 +0600

    create filetexts.txt file for input ouput operation  and then write read  operation in inputAndoutput file

--- File Input and Output/inputAndoutput.py
+++ File Input and Output/inputAndoutput.py
@@ -1,6 +1,16 @@
 # We have to open a file before reading or writing.
 
 
-# Syntax: variable=open(“file_name”, “mode”)
-# data=variable.read()
+# Syntax: variable=open(“file_name”, “mode”)  
+# data=variable.read()   [read is an operation.]
 # variable.close() # Must close the file.
+
+file=open('F:\\Drive D\\Python3\\file.texts\\filetexts.txt')
+da=file.read()
+print(da)
+file.close()
+
+file=open('F:\\Drive D\\Python3\\file.texts\\filetexts.txt','r')
+da=file.read()
+print(da)
+file.close()
