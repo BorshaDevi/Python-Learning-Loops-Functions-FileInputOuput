57732bf8243d9792057dac9bd750ece6b188cd75
Borsha Devi <borshadevi27@gmail.com>
2026-01-11 10:44:01 +0600

    Now I learned about with keyword which If we used then we do not need to write close method because it is automatically close the file

--- File Input and Output/inputAndoutput.py
+++ File Input and Output/inputAndoutput.py
@@ -16,3 +16,22 @@ print(da)
 file.close()
 
 
+# With keyword
+
+# with open(‘file name’,mode) as fi:
+#     fi.read/write()
+# fi means fi=open(‘file name’,mode)
+
+#  we use with,so there is no need to call file.close() , because with automatically closes the file.
+
+# " with " keyword  can be used for  all types of operation.
+
+with open('sample.txt','r') as f:
+    re=f.read()
+    print(re)
+
+with open('sample.txt','w')  as f:
+    f.write('Learn about with.')  
+
+
+
