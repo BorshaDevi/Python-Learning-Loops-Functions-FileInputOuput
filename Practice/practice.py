c1fe1a583b050f4c5f1b1fb7e26f6e2dd14087c6
Borsha Devi <borshadevi27@gmail.com>
2026-01-14 08:37:38 +0600

    Now i practice 23 question in the practice file

--- Practice/practice.py
+++ Practice/practice.py
@@ -277,4 +277,24 @@ check_for_line('learning')
 
 # 23. From a file containing numbers separated by comma, print the count of even numbers.
 
+with open ('practice.txt') as f:
+    data=f.read()
+    print(data)
+    num=''
+    for i in range(len(data)):
+        if(data[i] == ','):
+            print(int(num))
+            num=''
+        else:
+            num += data[i]    
 
+with open ('practice.txt') as f:
+    data=f.read()
+    print(data)
+    s_data=data.split(',')
+    print(s_data)
+    count=0
+    for i in s_data:
+        if(int(i)%2 == 0):
+            count +=1
+    print(count)
